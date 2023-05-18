8e2b23d875d1a4e8ad3e4d56b79dcc53df279d71
Ogoba David <dvdogoba23@gmail.comm>
2023-05-19 00:06:14 +0100

    added search result html page and made the search bar functional

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,24 +1,84 @@
-let appId = '8364506f'
-let apiKey = 'c621cb79027efe9e0eede35ed7f06cd2'
-const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const searchButton = document.querySelector('#searchButton');
+const searchInput = document.querySelector('#searchInput');
+let discoverNew = document.getElementById('discover');
 
-const url = `https://api.edamam.com/api/food-database/v2/parser?app_id=${appId}&api_key=${apiKey}`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c621cb79027efe9e0eede35ed7f06cd2',
-		'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
-	}
-};
-const foodData = async () => {
-    try {
-        const response = await fetch(corsProxyUrl + url, options) 
-        const result = await response.json();
-        console.log(result);
-    } catch (error) {
-        console.log(error);
-    }
-}
+import axios from "axios";
 
-console.log('edoye')
-foodData()
+// Function to handle search
+// function performSearch(searchTerm) {
+
+//   const options = {
+//     method: 'GET',
+//     url: `https://api.edamam.com/api/recipes/v2?type=public&q=${searchTerm}&app_id=8364506f&app_key=25208b6f52780cf1344c710f6a964801&random=true`
+//   };
+//   const searchData = async () => {
+//     try {
+//         const response = await axios(options);
+
+        // function renderResults() {
+        //     response.data.hits.forEach(hit => {
+        //         discoverNew.innerHTML += `
+        //         <div class="me-3">
+        //             <img src="${hit.recipe.image}" alt="" width="132" height="134" class="rounded image-fluid mb-3">
+        //             <h3 class="mb-1 fs-5">${hit.recipe.label}</h3>
+        //             <h4 class="text-orange fs-6 text-capitalize">${hit.recipe.cuisineType}</h4>
+        //         </div>
+        //         `
+        //     });
+        // }
+        // renderResults()
+
+        // console.log(response.data);
+//     } catch (error) {
+//         console.error(error);
+//     }
+//   }
+
+// searchData()
+
+//   console.log('Performing search for:', searchTerm);
+//   console.log(searchTerm)
+// }
+
+
+ // Event listener for search button click
+  searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm) {
+    window.location.href = `searchResults.html?query=${encodeURIComponent(searchTerm)}`;   
+  } else {
+    alert('Please enter a search term');
+  }
+  performSearch(searchTerm);
+});
+
+
+// Rendering random food on page load in the discover new div
+// const options = {
+//     method: 'GET',
+//     url: 'https://api.edamam.com/api/recipes/v2?type=public&app_id=8364506f&app_key=25208b6f52780cf1344c710f6a964801&dishType=Main%20course&random=true'
+//   };
+// const foodData = async () => {
+//     try {
+//         const response = await axios(options);
+//         function renderDiscover() {
+//             response.data.hits.forEach(hit => {
+//                 discoverNew.innerHTML += `
+//                 <div class="me-3">
+//                     <img src="${hit.recipe.image}" alt="" width="132" height="134" class="rounded image-fluid mb-3">
+//                     <h3 class="mb-1 fs-5">${hit.recipe.label}</h3>
+//                     <h4 class="text-orange fs-6 text-capitalize">${hit.recipe.cuisineType}</h4>
+//                 </div>
+//                 `
+//             });
+//         }
+        // renderDiscover()
+        // console.log(response.data);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// foodData()
+
+  
